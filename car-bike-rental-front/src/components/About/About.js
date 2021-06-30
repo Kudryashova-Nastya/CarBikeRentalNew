bdf2da2a63d4b4e6ceffe6f5eafaac7630b58a05
Кудряшова Настя <avk2001avk@gmail.com>
2021-06-30 05:53:10 +0300

    страница о нас готова

---
+++ car-bike-rental-front/src/components/About/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "bulma/css/bulma.min.css";
+import "./style.scss";
+
+function About() {
+  return (
+    <div>
+      <h1 className="title">О нас</h1>
+      <div className="container">
+        Классная компания аренды автомобилей и велосипедов в лице студентки 2
+        курса Кудряшовой Анастасии из группы 191-321
+      </div>
+    </div>
+  );
+}
+
+export default About;
